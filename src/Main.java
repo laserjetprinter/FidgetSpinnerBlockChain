630bec76961b8940c7badfe924b30fcacbb34336
laserjetprinter <laserjetprinter.cs@gmail.com>
2018-03-22 00:27:48 -0400

    Working with input.txt

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,12 @@
             block.
 */
 /***************************************************/
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Arrays;
+import java.io.FileNotFoundException;
 
 public class Main{
 
@@ -27,13 +32,45 @@ public class Main{
 	   a beginning block and hash exist.*/
 	String[] initialData = {"This is the first block - containing no data"};
 	BlockChain initialBlock = new BlockChain(0, initialData);
-	
+	BlockChain holding = initialBlock; //allows file to retrieve previous hash of initial 
+
+	/**Prints values of the current block*/
 	System.out.println("Initial block: ");
-	System.out.println("Data: ");
-	System.out.println(initialBlock.getData());
-	System.out.println("Current hash: "); 
+	System.out.print("Data: ");
+	System.out.println(Arrays.toString(initialData));
+	System.out.print("Current hash: "); 
 	System.out.println(initialBlock.getBlockHash());
-	System.out.println("Previous hash: ");
+	System.out.print("Previous hash: ");
 	System.out.println(initialBlock.getPrevHash());
+	System.out.println("");
+
+	/**Reads the input.txt file contents line by line*/
+	try (BufferedReader br = new BufferedReader(new FileReader("input.txt"))) {
+		String line;
+      		int count = 1;
+		BlockChain inputBlock;
+		while ((line = br.readLine()) != null) {
+
+		    String[] inputData = {line}; //creates input data from file
+		    inputBlock = new BlockChain(holding.getBlockHash(), inputData); //creates block
+
+		    /**Prints values of the current block*/
+		    System.out.println("Input block " + count + ": ");
+		    System.out.print("Data: ");
+		    System.out.println(Arrays.toString(inputData));
+		    System.out.print("Current hash: ");
+		    System.out.println(inputBlock.getBlockHash());
+		    System.out.print("Previous hash: ");
+		    System.out.println(inputBlock.getPrevHash());
+		    System.out.println();
+
+		    holding = inputBlock; //sets temp block to current, so next iteration has access to prev hash
+		    count++; 
+		}//end of while
+	    }//end of try
+	catch(FileNotFoundException e){
+	}//end of catch
+	catch(IOException e){
+	}//end of catch
     }//end of main
 }//end of Main
